c67e5a62e3f10f8e705f534a591fb74907d25276
tarek <tarekyousef440@gmail.com>
2025-05-17 19:02:01 +0300

    update login

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,9 +1,8 @@
 import { User } from "../javascript/models/User.js";
 
 const onSubmitForm = async (event) => {
-  event.preventDefault(); 
+  event.preventDefault();
 
-  
   document.getElementById("userNameErrorField").innerText = "";
   document.getElementById("passwordErrorField").innerText = "";
   document.getElementById("formError").innerText = "";
@@ -11,7 +10,6 @@ const onSubmitForm = async (event) => {
   const userName = document.getElementById("username").value.trim();
   const password = document.getElementById("password").value.trim();
 
-  
   if (!userName) {
     document.getElementById("userNameErrorField").innerText = "* Required";
     return;
@@ -21,23 +19,20 @@ const onSubmitForm = async (event) => {
     return;
   }
 
-  
-  const { user, errorCode, errorMessage } = await User.findUserByUsernameAndPassword(userName, password);
+  const { user, errorCode, errorMessage } =
+    await User.findUserByUsernameAndPassword(userName, password);
 
   if (user) {
-    
     localStorage.setItem("loggedInUser", JSON.stringify(user));
 
-    
     if (user.role === "donor") {
-      window.location.href = "/pages/donor-dashboard.html";
+      window.location.href = "/donor-dashboard.html";
     } else if (user.role === "student") {
-      window.location.href = "/pages/student-dashboard.html";
+      window.location.href = "/student-dashboard.html";
     } else {
-      window.location.href = "/pages/home.html";
+      window.location.href = "/admin-dashboard.html";
     }
   } else {
-    
     const formErrorField = document.getElementById("formError");
     if (errorCode === "userNotFound") {
       formErrorField.innerText = errorMessage;
@@ -49,11 +44,7 @@ const onSubmitForm = async (event) => {
   }
 };
 
-
 document.addEventListener("DOMContentLoaded", () => {
   const loginForm = document.getElementById("loginForm");
   loginForm.addEventListener("submit", onSubmitForm);
 });
-
-
-
